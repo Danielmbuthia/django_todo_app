de23fcd27bffa0eb6b3be6a41af7cafcddb49198
daniel <dambuthia@outlook.com>
2022-10-01 14:46:35 +0300

    django class based CRUD operations

--- todo_app/urls.py
+++ todo_app/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from .views import TodoCreateView, TodoDeleteView, TodoDetailView, TodoListView
+from .views import TodoCreateView, TodoDeleteView, TodoDetailView, TodoListView, TodoUpdateView
 
 urlpatterns = [
     path('',TodoListView.as_view(), name='home'),
-     path('detail/<pk>/',TodoDetailView.as_view(), name='detail'),
+    path('detail/<pk>/',TodoDetailView.as_view(), name='detail'),
+     path('edit/<pk>/',TodoUpdateView.as_view(), name='edit'),
     path('delete/<int:pk>/',TodoDeleteView.as_view(),name='delete'),
     path('create/',TodoCreateView.as_view(),name='create')
 ]
--- todo_app/views.py
+++ todo_app/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-from django.views.generic import ListView,DeleteView,DetailView,CreateView
+from django.views.generic import ListView,DeleteView,DetailView,CreateView,UpdateView
 from django.urls import reverse_lazy
 
 from todo_app.models import Todo
@@ -29,7 +29,9 @@ class TodoListView(ListView):
 
 class TodoDeleteView(DeleteView):
     model = Todo
+    context_object_name = 'todo'
     template_name = "todo/delete.html"
+    success_url = reverse_lazy('todo_app:home')
     
 
 class TodoDetailView(DetailView):
@@ -41,6 +43,14 @@ class TodoCreateView(CreateView):
     model = Todo
     fields = "__all__"
     template_name = "todo/todo_form.html"
-    success_url = reverse_lazy('home')
+    success_url = reverse_lazy('todo_app:home')
+    
+class TodoUpdateView(UpdateView):
+    model = Todo
+    fields = "__all__"
+    template_name = "todo/todo_form.html"
+    success_url = reverse_lazy('todo_app:home')
+    
+
     
 
