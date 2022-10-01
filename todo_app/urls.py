a4b5a647deb3a6dcd228895edcb9961f7f7598ff
daniel <dambuthia@outlook.com>
2022-10-01 19:14:57 +0300

    login and logout

--- todo_app/urls.py
+++ todo_app/urls.py
@@ -1,11 +1,16 @@
 from django.urls import path
 
-from .views import TodoCreateView, TodoDeleteView, TodoDetailView, TodoListView, TodoUpdateView
+from .views import CustomLoginView, TodoCreateView, TodoDeleteView, TodoDetailView, TodoListView, TodoUpdateView
+
+from django.contrib.auth.views import LogoutView
 
 urlpatterns = [
+    path('login/',CustomLoginView.as_view(),name='login'),
+    path('logout/',LogoutView.as_view(),name='logout'),
+    
     path('',TodoListView.as_view(), name='home'),
     path('detail/<pk>/',TodoDetailView.as_view(), name='detail'),
-     path('edit/<pk>/',TodoUpdateView.as_view(), name='edit'),
+    path('edit/<pk>/',TodoUpdateView.as_view(), name='edit'),
     path('delete/<int:pk>/',TodoDeleteView.as_view(),name='delete'),
     path('create/',TodoCreateView.as_view(),name='create')
 ]
--- todo_app/views.py
+++ todo_app/views.py
@@ -2,9 +2,14 @@ from django.shortcuts import render, redirect
 from django.views.generic import ListView,DeleteView,DetailView,CreateView,UpdateView
 from django.urls import reverse_lazy
 
+from django.contrib.auth.views import LoginView
+from django.contrib.auth.mixins import LoginRequiredMixin
+
 from todo_app.models import Todo
 
 # Create your views here.
+
+## auth functionality
 
 # def home(request):
 #     if request.method == 'POST':
@@ -22,35 +27,41 @@ from todo_app.models import Todo
 #     todo.delete()
 #     return redirect('/')
 
-class TodoListView(ListView):
+
+class CustomLoginView(LoginView):
+    template_name = 'todo/login.html'
+    fields = '__all__'
+    redirect_authenticated_user = True
+    
+    def get_success_url(self):
+        return reverse_lazy('todo_app:home')
+
+class TodoListView(LoginRequiredMixin,ListView):
     model = Todo
     template_name = "todo/todo_list.html"
     context_object_name = 'todos'
 
-class TodoDeleteView(DeleteView):
+class TodoDeleteView(LoginRequiredMixin,DeleteView):
     model = Todo
     context_object_name = 'todo'
     template_name = "todo/delete.html"
     success_url = reverse_lazy('todo_app:home')
     
 
-class TodoDetailView(DetailView):
+class TodoDetailView(LoginRequiredMixin,DetailView):
     model = Todo
     template_name = "todo/detail.html"
     context_object_name = 'todo'
     
-class TodoCreateView(CreateView):
+class TodoCreateView(LoginRequiredMixin,CreateView):
     model = Todo
     fields = "__all__"
     template_name = "todo/todo_form.html"
     success_url = reverse_lazy('todo_app:home')
     
-class TodoUpdateView(UpdateView):
+class TodoUpdateView(LoginRequiredMixin,UpdateView):
     model = Todo
     fields = "__all__"
     template_name = "todo/todo_form.html"
     success_url = reverse_lazy('todo_app:home')
     
-
-    
-
